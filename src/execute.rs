b8d41c6707dfeecbea1a55d6e47afcfaa061035b
cartoon-raccoon <shaughn.chan@gmail.com>
2020-10-11 17:39:18 +0800

    Added execute function
    Basic functionality of the shell has been established.
    Next is implementing all the features.

---
+++ src/execute.rs
@@ -0,0 +1,30 @@
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+
+use nix::sys::signal::{signal, Signal, SigHandler,};
+
+use crate::parser::ParsedCmd;
+
+#[must_use]
+pub fn execute(cmd: ParsedCmd) -> bool {
+    let mut command = Command::new(cmd.cmd);
+    if let Ok(mut child) = unsafe { 
+        command
+        .args(cmd.args)
+        .pre_exec(|| {
+                match signal(Signal::SIGINT, SigHandler::SigDfl) {
+                    Ok(_) => {},
+                    Err(_) => {panic!("Could not set SIGINT handler")}
+                };
+                match signal(Signal::SIGQUIT, SigHandler::SigDfl) {
+                    Ok(_) => {},
+                    Err(_) => {panic!("Could not set SIGQUIT handler")}
+                }
+            Ok(())
+        }).spawn() } {
+            child.wait().expect("Child wasn't running").success()
+    } else {
+        eprintln!("Command not found!");
+        true
+    }
+}
