113314f4aedd9ed78e0fe75640a8176a96db3d73
cartoon-raccoon <shaughn.chan@gmail.com>
2020-10-18 17:48:45 +0800

    Implemented framework for shell execution
    Main framework for execution is in place.
    Pipes don't work completely yet, but are being fixed.
    
    Todo: Add a function that passes terminal control to the process group.

--- src/core.rs
+++ src/core.rs
@@ -1,14 +1,31 @@
 use std::os::unix::io::RawFd;
+use std::process;
+use std::ffi::{CString, CStr};
 
 use nix::unistd::{
+    Pid,
+    getpid,
+    setpgid,
     isatty,
     fork, 
     pipe, 
     execve, 
     dup2, 
+    close,
     ForkResult
 };
 use nix::Result;
+use nix::Error;
+use nix::errno::Errno;
+
+//placeholder - move to job control
+use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
+
+//placeholder imports for bugfixing
+use nix::unistd::execvp;
+use std::fs::File;
+use std::os::unix::io::FromRawFd;
+use std::io::{Read, Write};
 
 use crate::types::*;
 use crate::shell::Shell;
@@ -43,8 +60,10 @@ pub fn run_pipeline(
 
     let isatty = isatty(1)?;
 
-    
-    let mut idx: i32 = 0;
+    let mut pgid = Pid::from_raw(0);
+    let mut idx: usize = 0;
+    let mut children = Vec::new();
+
     for cmd in &job.cmds {
 
         let params = CommandParams{
@@ -54,23 +73,46 @@ pub fn run_pipeline(
             env: shell.env.clone(),
         };
 
-        run_command(
+        let childpid = run_command(
             cmd, 
             idx, 
+            &mut pgid,
             &pipes,
             params,
             &mut term_given,
             &mut cmdresult,
         )?;
+
+        children.push(childpid);
         idx += 1;
     }
+
+    //placeholder code - waiting will be moved to job control
+    for childpid in children {
+        match waitpid(Pid::from_raw(childpid), Some(WaitPidFlag::WUNTRACED))? {
+            WaitStatus::Exited(pid, status) => {
+                println!("process {} exited with status {}", pid, status);
+            }
+            WaitStatus::Signaled(pid, signal, cd) => {
+                println!("process {} was signaled by {}: coredump? {}", pid, signal, cd);
+            }
+            WaitStatus::Stopped(pid, signal) => {
+                println!("process {} was stopped by {}", pid, signal);
+            }
+            status @ _ => {
+                println!("process {} has status {:?}", childpid, status);
+            }
+        }
+    }
+
     Ok((term_given, CommandResult::new()))
 }
 
 /// This is one deep-ass core function.
 fn run_command(
     cmd: &Cmd, 
-    idx: i32, 
+    idx: usize, 
+    pgid: &mut Pid,
     pipes: &Vec<(RawFd, RawFd)>,
     params: CommandParams,
     term_given: &mut bool,
@@ -80,6 +122,7 @@ fn run_command(
     //*Fork!
     // 
     //If in child: ========================================
+    // 0. Set the pgid
     //
     // 1. If idx > 0 grab the appropriate pipe from pipes
     //    Hook up stdin (0) to pipe using dup2():
@@ -124,18 +167,146 @@ fn run_command(
     let mut fds_capture_stdout = pipe()?;
     let mut fds_capture_stderr = pipe()?;
 
+    println!("Command position: {}", idx);
+    
+    let pipes_count = pipes.len();
     let forkresult = fork()?;
     match forkresult {
         ForkResult::Child => {
+            //setting process groups
+            let pid = getpid();
             if idx == 0 {
-
+                *pgid = pid; //setting pgid to own pid
+                match setpgid(Pid::from_raw(0), pid) { //setting pgid to own pid
+                    Ok(()) => { //* Debug code vvv (remove this after use)
+                        println!("Running {} with pid {} and pgid {}", cmd.cmd, pid, pgid);
+                    },
+                    Err(e) => {
+                        eprintln!("{}: failed to set pgid of process pid {} and pgid {}", e, pid, pgid);
+                        process::exit(1);
+                    }
+                }
+            } else {
+                match setpgid(Pid::from_raw(0), *pgid) {
+                    Ok(()) => {
+                        println!("Running {} with pid {} and pgid {}", cmd.cmd, pid, pgid);
+                    },
+                    Err(e) => {
+                        eprintln!("{}: failed to set pgid of process pid {} and pgid {}", e, pid, pgid);
+                        process::exit(1);
+                    }
+                }
             }
+
+            //*BUG: NOT FUCKING READING PROPERLY FROM STDIN IDFK WHY
+
+            //connecting up pipes for commands to read from
+            if idx > 0 { //not the first command
+                eprintln!("Connecting stdin to pipe from command {} pid {}", idx, pid);
+                match dup2(pipes[idx - 1].0, 0) {
+                    Ok(_) => {},
+                    Err(e) => {
+                        eprintln!("{}, failed to connect pipe for process {}", e, pid);
+                        process::exit(1);
+                    }
+                }
+                eprintln!("Closing pipe read end");
+                match close(pipes[idx - 1].0) {
+                    Ok(()) => {},
+                    Err(e) => {
+                        eprintln!("{}, failed to connect pipe for process {}", e, pid);
+                        process::exit(1);
+                    }
+                }
+                // let mut fout = unsafe { File::from_raw_fd(0) };
+                // let mut sout = String::new();
+                // eprintln!("Reading to string from process {}", pid);
+                // fout.read_to_string(&mut sout).expect("Could not read to string");
+                // eprintln!("Pulled from pipe: {}", sout);
+            }
+
+            //connecting up pipes for commands to write to
+            if idx < pipes_count {
+                eprintln!("Connecting stdout to pipe from command {} pid {}", idx, pid);
+                match dup2(pipes[idx].1, 1) {
+                    Ok(_) => {
+                        // eprintln!("Writing to file descriptor {}", fd);
+                        // std::io::stdout().write(b"Hello\n").unwrap();
+                        // std::io::stdout().flush().unwrap();
+                    },
+                    Err(e) => {
+                        eprintln!("{}, failed to connect pipe for process {}", e, pid);
+                        process::exit(1);
+                    }
+                }
+                eprintln!("Closing pipe write end");
+                match close(pipes[idx].1) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("could not close filedesc: {}", e);
+                    }
+                }
+                match close(1) {
+                    Ok(_) => {},
+                    Err(e) => {eprintln!("Could not close stdout: {}", e);}
+                }
+            }
+
+            //TODO 1: Handle redirects
+
+            //TODO 2: Check for builtin commands
+
+            //TODO 3: Redirecting output to output capture
+
+            //TODO: Load in env vars
+            //TODO: Search in path
+            let cmdstring = cmd.cmd.clone();
+            let c_cmd = CString::new(cmdstring.as_str()).unwrap();
+            let args: Vec<CString> = cmd.args.clone().into_iter()
+                .map(|arg| {
+                    match CString::new(arg.as_str()) {
+                        Ok(c_arg) => c_arg,
+                        Err(_) => {
+                            eprintln!("oyster: could not process command arguments");
+                            process::exit(0);
+                        }
+                    }
+                }).collect();
+            let c_args: Vec<&CStr> = args.iter().map(|arg| arg.as_c_str()).collect();
+            match execvp(&c_cmd, c_args.as_slice()) {
+                Ok(_) => {}
+                Err(e) => {
+                    match e {
+                        Error::Sys(Errno::ENOEXEC) => {
+                            eprintln!("oyster: exec format error");
+                        }
+                        Error::Sys(Errno::ENOENT) => {
+                            eprintln!("oyster: command {} not found", cmd.cmd);
+                        }
+                        Error::Sys(Errno::EACCES) => {
+                            eprintln!("oyster: permission denied");
+                        }
+                        _ => {
+                            eprintln!("oyster: error: {:?}", e);
+                        }
+                    }
+                }
+            }
+
+            process::exit(1);
         }
         ForkResult::Parent{child,..} => {
+            if idx == 0 {
+                *pgid = child; //setting pgid to set for subsequent forks
+            }
+            match setpgid(child, *pgid) {
+                Ok(()) => {}
+                Err(e) => { eprintln!("Could not set child pgid from parent: {}", e); }
+            }
+
             let child_pid: i32 = child.into();
+
+            return Ok(child_pid);
         }
     }
-
-    Ok(0)
-
 }
--- src/execute.rs
+++ src/execute.rs
@@ -1,30 +1,93 @@
-use std::os::unix::process::CommandExt;
-use std::process::Command;
+use std::error::Error;
 
-use nix::sys::signal::{signal, Signal, SigHandler,};
+use crate::types::{Token, Job, Exec};
+use crate::parser::Lexer;
+use crate::core;
+use crate::shell::Shell;
 
-use crate::parser::Cmd;
 
-#[must_use]
-pub fn execute(cmd: Cmd) -> bool {
-    let mut command = Command::new(cmd.cmd);
-    if let Ok(mut child) = unsafe { 
-        command
-        .args(cmd.args)
-        .pre_exec(|| {
-                match signal(Signal::SIGINT, SigHandler::SigDfl) {
-                    Ok(_) => {},
-                    Err(_) => {panic!("Could not set SIGINT handler")}
-                };
-                match signal(Signal::SIGQUIT, SigHandler::SigDfl) {
-                    Ok(_) => {},
-                    Err(_) => {panic!("Could not set SIGQUIT handler")}
+/// High level control of all jobs. Conditional execution is handled here.
+/// Parses tokens into jobs, performs expansion and executes them.
+pub fn execute_jobs(shell: &mut Shell, tokens: Vec<Token>) -> Result<(), Box<dyn Error>> {
+    /*
+    * Step 1: Match on exec condition
+    * Step 2: Pass job to execute()
+    */
+    let jobs = Lexer::parse_tokens(tokens)?;
+    println!("{:?}", jobs);
+
+    //* perform all expansions here
+    //* this will alter the job structs
+
+    let mut execif: Option<Exec>;
+
+    for job in jobs {
+        execif = job.execnext;
+        if let Some(execcond) = execif {
+            match execcond {
+                Exec::And => { //continue if last job succeeded
+                    if execute(shell, job, false) {
+                        continue;
+                    } else {
+                        //return error
+                    }
                 }
-            Ok(())
-        }).spawn() } {
-            child.wait().expect("Child wasn't running").success()
-    } else {
-        eprintln!("Command not found!");
-        true
+                Exec::Or => { //continue if last job failed
+                    if !execute(shell, job, false) {
+                        continue;
+                    } else {
+                        //return error
+                    }
+                }
+                Exec::Consec => { //unconditional execution
+                    execute(shell, job, true);
+                    continue;
+                }
+                Exec::Background => { //run jobs asynchronously
+
+                }
+            }
+        } else { //if is None; this should only occur on the last job
+            execute(shell, job, false);
+        }
     }
+    Ok(())
+}
+
+/// Lower level control. Executes single pipeline.
+/// Checks for builtins without pipeline
+pub fn execute(shell: &mut Shell, job: Job, background: bool) -> bool {
+
+    if job.cmds.len() == 1 { //no pipeline
+        match job.cmds[0].cmd.as_str() {
+            "cd" => {
+                return true;
+            }
+            "which" => {
+                return true;
+            }
+            "eval" => {
+                return true;
+            }
+            "source" => {
+                return true;
+            }
+            "export" => {
+                return true;
+            }
+            "echo" => {
+                return true;
+            }
+            "kill" => {
+                return true;
+            }
+            "exit" => {
+                return true;
+            }
+            _ => {}
+        }
+    }
+    
+    core::run_pipeline(shell, job, background, false);
+    true
 }
