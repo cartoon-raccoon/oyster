6a1d3e1b628cc41966c76165ac98b201656b9d3c
cartoon-raccoon <shaughn.chan@gmail.com>
2020-11-02 20:28:58 +0800

    Implemented basic scripting infrastructure
    The basic infrastructure of the code block and the for loop are in place, albeit untested
    The entire thing is based around trait objects and dynamic dispatch, and every block implements ShellConstruct.
    Might shift this to generics-based if needed.

--- src/scripting.rs
+++ src/scripting.rs
@@ -1,9 +1,63 @@
-use crate::types::Job;
-use crate::shell::Shell;
+use std::collections::HashMap;
 
+use crate::types::{Job, ShellError};
+use crate::shell::Shell;
+use crate::execute::execute_jobs;
+
+pub trait ShellConstruct {
+    fn execute(
+        &mut self, 
+        shell: &mut Shell, 
+        vars: &mut HashMap<String, String>
+    ) -> Result<i32, ShellError>;
+}
+
+//impl ShellConstruct
+pub struct CodeBlock {
+    jobs: Vec<Job>,
+}
+
+impl ShellConstruct for CodeBlock {
+    fn execute(
+        &mut self, 
+        shell: &mut Shell, 
+        vars: &mut HashMap<String, String>) 
+        -> Result<i32, ShellError> {
+        
+        for (key, value) in vars {
+            shell.add_variable(key, value);
+        }
+        let (status, cap) = execute_jobs(shell, self.jobs.clone(), false)?;
+        Ok(status)
+    }
+}
+
+//impl ShellConstruct
 pub struct ForLoop {
-    //loop_over: ???
-    jobs: Vec<Job>
+    loop_var: String,
+    loop_over: Vec<String>,
+    codeblocks: Vec<Box<dyn ShellConstruct>>
+}
+
+impl ShellConstruct for ForLoop {
+    fn execute(
+        &mut self, 
+        shell: &mut Shell, 
+        vars: &mut HashMap<String, String>) 
+        -> Result<i32, ShellError> {
+
+        let mut status: i32 = 0;
+        //TODO: fix the fucking clones
+        for item in &mut self.loop_over {
+            let entry = vars.entry(self.loop_var.clone())
+                .or_insert(self.loop_var.clone());
+            *entry = item.clone();
+            for code in &mut self.codeblocks {
+                status = code.execute(shell, vars)?;
+            }
+        }
+        Ok(status)
+    }
 }
 
 impl ForLoop {
